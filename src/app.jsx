827f9affda4fbc6231f71cd30ae3aac60520bf40
Alissa Schlecht <schlecht220@gmail.com>
2017-02-20 20:39:26 +0000

    add component

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import Header from './header.jsx';
 
-export default class App extends React.Component {
+// export default class App2 extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>It Works!</h1>
+//         <p>This React project just works including <span className="redBg">module</span> local styles.</p>
+//         <p>Enjoy!</p>
+//       </div>
+//     )
+//   }
+// }
+
+
+export default class ShoppingList extends React.Component {
+  
   render() {
     return (
-      <div>
-        <h1>It Works!</h1>
-        <p>This React project just works including <span className="redBg">module</span> local styles.</p>
-        <p>Enjoy!</p>
+      <div className="shopping-list">
+        <Header/>
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
       </div>
-    )
+    );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import App from './app.jsx';
+import ShoppingList from './app.jsx';
 
-render( <AppContainer><App/></AppContainer>, document.querySelector("#app"));
 
-if (module && module.hot) {
-  module.hot.accept('./app.jsx', () => {
-    const App = require('./app.jsx').default;
-    render(
-      <AppContainer>
-        <App/>
-      </AppContainer>,
-      document.querySelector("#app")
-    );
-  });
-}
+
+render( <AppContainer><ShoppingList name="Mark"/></AppContainer>, document.querySelector("#app"));
+
+
+
+
+
+
+
+// if (module && module.hot) {
+//   module.hot.accept('./app.jsx', () => {
+//     render(
+//       <AppContainer>
+//         <ShoppingList/>
+//       </AppContainer>,
+//       document.querySelector("#app")
+//     );
+//   });
+// }
